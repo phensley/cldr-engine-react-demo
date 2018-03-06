c1246364601d8289329c25e9cbf789854a0a9555
Patrick Hensley <pathensley@gmail.com>
2018-03-06 14:31:50 -0500

    Add script to copy cldr resource packs to public folder.

---
+++ scripts/copy-packs.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { basename, join } = require('path');
+const zlib = require('zlib');
+
+const mtime = f => fs.statSync(f).mtimeMs;
+const newerThan = (a, b) => mtime(a) > mtime(b);
+
+const main = () => {
+  const src = join(__dirname, '../node_modules/@phensley/cldr/packs');
+  const dst = join(__dirname, '../public/packs');
+  if (!fs.existsSync(dst)) {
+    fs.mkdirSync(dst);
+  }
+
+  if (!fs.existsSync(src)) {
+    console.error('install @phensley/cldr!');
+    process.exit(1);
+  }
+
+  const names = fs.readdirSync(src)
+    .filter(n => n.endsWith('.json.gz'))
+    .map(n => join(src, n));
+
+  if (names.length === 0) {
+    console.error('no packs are available.. reinstall @phensley/cldr');
+    process.exit(1);
+  }
+
+  names.forEach(path => {
+    const name = basename(path, '.json.gz');
+    const outpath = join(dst, `${name}.json`);
+    if (fs.existsSync(outpath) && newerThan(outpath, path)) {
+      return;
+    }
+
+    console.warn(`copying ${outpath}..`);
+    const compressed = fs.readFileSync(path);
+    const raw = zlib.gunzipSync(compressed).toString('utf-8');
+    fs.writeFileSync(outpath, raw);
+  });
+};
+
+main();
