209bd1f0dbec9e2585cef08eb9c5fcf90a4eb388
Patrick Hensley <pathensley@gmail.com>
2018-10-19 14:41:25 -0400

    Example of webpack dynamic import

--- src/locale/setup.ts
+++ src/locale/setup.ts
@@ -1,15 +1,6 @@
 const start = +(new Date());
 
-import wretch from 'wretch';
 import { CLDRFramework, CLDROptions } from '@phensley/cldr';
-
-// Import the resource file containing information about the resource packs
-import Resource from '@phensley/cldr/packs/resource.json';
-
-// Copy the sha256 hash of all of the packages, to use for cache busting.
-// Note: Resource files are be copied by the build process with the
-// matching version in the path.
-const version = Resource.sha256.substring(0, 10);
 
 // Import default language directly so it's always available
 import EnglishPack from '@phensley/cldr/packs/en.json';
@@ -23,9 +14,8 @@ const asyncLoader = (language: string): Promise<any> => {
     if (language === 'en') {
       resolve(EnglishPack);
     }
-    wretch(`${process.env.PUBLIC_URL}/packs/${language}-${version}.json`)
-      .get()
-      .json(resolve)
+    import(/* webpackChunkName: "cldr-pack-" */ `@phensley/cldr/packs/${language}.json`)
+      .then(resolve)
       .catch(reject);
   });
 };
