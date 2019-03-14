31bd08db2670701254901fbf5ecdd5fe639cc933
Patrick Hensley <pathensley@gmail.com>
2019-03-14 11:30:20 -0400

    Upgrade to 0.8.3, expose version in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
+import { CLDRFramework } from '@phensley/cldr';
 
 export class Nav extends React.Component<any> {
 
   render(): JSX.Element {
+    const version = CLDRFramework.version();
     return (
       <nav className='nav'>
         <div className='container'>
-          <a href='https://github.com/phensley/cldr-engine' className='pagename'>@phensley/cldr</a>
-          <a href='https://github.com/phensley/cldr-engine-react-demo'>Github</a>
+          <a href='https://github.com/phensley/cldr-engine' className='pagename'>@phensley/cldr {version}</a>
+          <a href='https://github.com/phensley/cldr-engine-react-demo'>Demo Github</a>
         </div>
       </nav>
     );
