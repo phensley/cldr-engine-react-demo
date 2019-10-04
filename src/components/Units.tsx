9da6e0c552b86692db00b98e43dfcc5b7cf7fb34
Patrick Hensley <pathensley@gmail.com>
2019-10-04 18:24:08 -0400

    Upgrade to 0.19.1, update unit examples

--- src/components/Units.tsx
+++ src/components/Units.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
-import { CLDR, UnitFormatOptions, UnitType } from '@phensley/cldr';
+import { CLDR, Quantity, UnitFormatOptions, UnitType } from '@phensley/cldr';
 import { State } from '../reducers';
 import { renderOptions } from './utils';
 
@@ -10,31 +10,34 @@ interface Props {
   cldr: CLDR;
 }
 
-const UNITS: UnitType[] = [
-  'foot', 'kilogram', 'light-year', 'mile', 'cubic-meter'
-];
-
-const OPTIONS: UnitFormatOptions[] = [
-  { group: true },
-  { length: 'short' },
-  { length: 'narrow' },
-  { style: 'short', length: 'short', minimumSignificantDigits: 3 },
-];
-
 const NUMBERS: string[] = [
   '.9997',
   '12345.6789',
   '1200599.3999'
 ];
 
-const formatQuanties = (cldr: CLDR, unit: UnitType, opts: UnitFormatOptions): JSX.Element => {
-  const elems: JSX.Element[] = [];
-  NUMBERS.forEach((value, i) => {
-    const s1 = cldr.Units.formatQuantity({ value, unit }, opts);
-    const s2 = cldr.Units.formatQuantity({ value, unit, per: 'second' }, opts);
-    elems.push(<span key={i}>{i > 0 ? <br/> : ''}{s1}<br/>{s2}</span>);
-  });
-  return <span className='unit'>{elems}</span>;
+const QUANTITY: Quantity[] = [];
+
+NUMBERS.forEach(value => {
+  QUANTITY.push({ value, unit: 'foot' });
+  QUANTITY.push({ value, unit: 'kilogram' });
+  QUANTITY.push({ value, unit: 'pound', per: 'square-inch' });
+  QUANTITY.push({ value, unit: 'gigabyte', per: 'second' });
+  QUANTITY.push({ value, unit: 'light-year' });
+  QUANTITY.push({ value, unit: 'newton', times: 'meter' });
+  QUANTITY.push({ value, unit: 'foot', times: 'pound' });
+});
+
+const OPTIONS: UnitFormatOptions[] = [
+  { group: true },
+  { length: 'narrow' },
+  { length: 'short' },
+  { style: 'short', length: 'short', minimumSignificantDigits: 3 },
+];
+
+const formatQuanties = (cldr: CLDR, qty: Quantity, opts: UnitFormatOptions): JSX.Element => {
+  const s = cldr.Units.formatQuantity(qty, opts);
+  return <span className='unit'>{s}</span>;
 };
 
 class UnitsImpl extends React.Component<Props> {
@@ -47,10 +50,10 @@ class UnitsImpl extends React.Component<Props> {
 
   units(): JSX.Element[] {
     const { cldr } = this.props;
-    return UNITS.map((u, i) => {
+    return QUANTITY.map((qty, i) => {
       return (
         <tr key={i}>
-          {OPTIONS.map((o, j) => <td key={j}>{formatQuanties(cldr, u, o)}</td>)}
+          {OPTIONS.map((o, j) => <td key={j}>{formatQuanties(cldr, qty, o)}</td>)}
         </tr>
       );
     });
