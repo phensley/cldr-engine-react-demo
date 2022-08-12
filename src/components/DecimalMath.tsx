6c778fb4e61e22a79f6cac73f437e72a4258a895
Patrick Hensley <pathensley@gmail.com>
2022-08-12 08:52:35 -0400

    Upgrade to 1.5.1; upgrade all dependencies

--- src/components/DecimalMath.tsx
+++ src/components/DecimalMath.tsx
@@ -1,24 +1,24 @@
-import * as React from 'react';
-import { bindActionCreators, AnyAction, Dispatch } from 'redux';
-import { connect } from 'react-redux';
-import { Decimal, DecimalConstants } from '@phensley/cldr';
+import * as React from "react";
+import { bindActionCreators, AnyAction, Dispatch } from "redux";
+import { connect } from "react-redux";
+import { Decimal, DecimalConstants } from "@phensley/cldr";
 
-import { State } from '../reducers';
-import { mathChangeA, mathChangeB } from '../actions';
+import { State } from "../reducers";
+import { mathChangeA, mathChangeB } from "../actions";
 
 const SCALE_1 = 3;
 const SCALE_2 = 20;
 
-const DEFAULT_A = '123.45';
-const DEFAULT_B = '0.0009';
+const DEFAULT_A = "123.45";
+const DEFAULT_B = "0.0009";
 
 type Pair = [string, string];
 
 const NUMBERS: Pair[] = [
-  ['-10', '-6'],
-  ['7.365', '0'],
-  ['-5', '17.311'],
-  ['15.7e20', '3.1415926535897932384']
+  ["-10", "-6"],
+  ["7.365", "0"],
+  ["-5", "17.311"],
+  ["15.7e20", "3.1415926535897932384"],
 ];
 
 const calculate = (as: string, bs: string, scale: number): JSX.Element => {
@@ -29,7 +29,7 @@ const calculate = (as: string, bs: string, scale: number): JSX.Element => {
   try {
     a = new Decimal(as);
     b = new Decimal(bs);
-  } catch (error) {
+  } catch (error: any) {
     elems.push(<span key={i++}>{error.toString()}</span>);
     return <code>{elems}</code>;
   }
@@ -37,34 +37,38 @@ const calculate = (as: string, bs: string, scale: number): JSX.Element => {
   // precision for math operations
   const ctx = { precision: 100 };
   const emit = (op: string, d: Decimal) =>
-    elems.push(<span key={i++}>A{op}B&nbsp;=&nbsp;{d.setScale(scale).toString()}<br/></span>);
+    elems.push(
+      <span key={i++}>
+        A{op}B&nbsp;=&nbsp;{d.setScale(scale).toString()}
+        <br />
+      </span>
+    );
 
-  emit('+', a.add(b));
-  emit('-', a.subtract(b));
-  emit('*', a.multiply(b, ctx));
+  emit("+", a.add(b));
+  emit("-", a.subtract(b));
+  emit("*", a.multiply(b, ctx));
   try {
     const r = a.divide(b, ctx);
-    emit('/', r);
-  } catch (error) {
+    emit("/", r);
+  } catch (error: any) {
     elems.push(<span key={i++}>{error.toString()}</span>);
   }
   return <code>{elems}</code>;
 };
 
 export class DecimalMathImpl extends React.Component<any> {
-
   onChange = (e: React.FormEvent<HTMLInputElement>): void => {
     const { id, value } = e.currentTarget;
-    if (id === 'userA') {
+    if (id === "userA") {
       this.props.actions.mathChangeA(value || DEFAULT_A);
     } else {
       this.props.actions.mathChangeB(value || DEFAULT_B);
     }
-  }
+  };
 
   headings(): JSX.Element {
     return (
-        <tr>
+      <tr>
         <td>A</td>
         <td>B</td>
         <td>scale={SCALE_1}</td>
@@ -76,25 +80,41 @@ export class DecimalMathImpl extends React.Component<any> {
   operations(): JSX.Element[] {
     const { userA, userB } = this.props;
     const max = NUMBERS.length;
-    const res = !NUMBERS ? [] : NUMBERS.map((pair, i) => {
-      const [a, b] = pair;
-      return (
-        <tr key={i}>
-          <td>{a}</td>
-          <td>{b}</td>
-          <td>{calculate(a, b, SCALE_1)}</td>
-          <td>{calculate(a, b, SCALE_2)}</td>
-        </tr>
-      );
-    });
-    res.push((
+    const res = !NUMBERS
+      ? []
+      : NUMBERS.map((pair, i) => {
+          const [a, b] = pair;
+          return (
+            <tr key={i}>
+              <td>{a}</td>
+              <td>{b}</td>
+              <td>{calculate(a, b, SCALE_1)}</td>
+              <td>{calculate(a, b, SCALE_2)}</td>
+            </tr>
+          );
+        });
+    res.push(
       <tr key={max}>
-        <td><input type='text' id='userA' placeholder={DEFAULT_A} onChange={this.onChange} /></td>
-        <td><input type='text' id='userB' placeholder={DEFAULT_B} onChange={this.onChange} /></td>
+        <td>
+          <input
+            type="text"
+            id="userA"
+            placeholder={DEFAULT_A}
+            onChange={this.onChange}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            id="userB"
+            placeholder={DEFAULT_B}
+            onChange={this.onChange}
+          />
+        </td>
         <td>{calculate(userA, userB, SCALE_1)}</td>
         <td>{calculate(userA, userB, SCALE_2)}</td>
       </tr>
-    ));
+    );
     return res;
   }
 
@@ -102,9 +122,9 @@ export class DecimalMathImpl extends React.Component<any> {
     return (
       <div>
         <h1>Arbitrary-precision math</h1>
-        <table className='table'>
-          <thead className='options'>{this.headings()}</thead>
-          <tbody className='math'>{this.operations()}</tbody>
+        <table className="table">
+          <thead className="options">{this.headings()}</thead>
+          <tbody className="math">{this.operations()}</tbody>
         </table>
       </div>
     );
@@ -113,11 +133,11 @@ export class DecimalMathImpl extends React.Component<any> {
 
 const mapState = (s: State) => ({
   userA: s.math.userA,
-  userB: s.math.userB
+  userB: s.math.userB,
 });
 
 const mapDispatch = (d: Dispatch<AnyAction>) => ({
-  actions: bindActionCreators({ mathChangeA, mathChangeB }, d)
+  actions: bindActionCreators({ mathChangeA, mathChangeB }, d),
 });
 
 export const DecimalMath = connect(mapState, mapDispatch)(DecimalMathImpl);
